e6f5b5fa1ba8e04c85dc723d202bd2803c4587fe
Dennis <ngetichdennis90@gmail.com>
2024-05-07 16:50:37 +0300

    curated Login form using modal

--- fundraiser/src/components/Home/Login.jsx
+++ fundraiser/src/components/Home/Login.jsx
@@ -1,7 +1,47 @@
+import React from 'react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, FormControl, FormLabel, Input } from "@chakra-ui/react";
+
 export default function Login(){
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const initialRef = React.useRef(null);
+    const finalRef = React.useRef(null);
+  
     return(
         <div>
-            <h1>Login</h1>
+      <Button onClick={onOpen}>Login</Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Login</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Username</FormLabel>
+              <Input ref={initialRef} placeholder="Username" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="Password" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Login
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
         </div>
             )
 }
