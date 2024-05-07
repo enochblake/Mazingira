7b1db191e236689ecde71f2d55d1e10323847221
Dennis <ngetichdennis90@gmail.com>
2024-05-07 16:36:54 +0300

    curated component boilerplates and also initiated all the files while linking them to the parent component

---
+++ fundraiser/src/components/Home/homesection.jsx
@@ -0,0 +1,7 @@
+export default function HomeSection(){
+    return(
+        <div>
+            <h1>Home section</h1>
+        </div>
+            )
+}
---
+++ fundraiser/src/components/Home/navbar.jsx
@@ -0,0 +1,8 @@
+import Login from "./Login"
+export default function Navbar(){
+    return(
+        <div>
+            <Login/>
+        </div>
+            )
+}
