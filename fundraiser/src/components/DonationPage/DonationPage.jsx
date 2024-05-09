3580cf8154362fc7c59caeddda94ee3422dd79d7
Priscilla-22 <priscillamw123@gmail.com>
2024-05-09 14:09:53 +0300

    refactor:files and folders

--- fundraiser/src/components/DonationPage/DonationPage.jsx
+++ fundraiser/src/components/DonationPage/DonationPage.jsx
@@ -1,6 +1,6 @@
 // Components/DonationPage/DonationPage.jsx
 import React from 'react';
-import DonationForm from './DonationForm';
+import DonationForm from '../DonationsPay/DonationForm';
 import Testimonials from './Testimonials';
 import OrganizationDetails from '../Organizitons/OrganizationDetails';
 
