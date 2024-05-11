b36d431bbdd6659c17f21c123fda4fa9b817dfd1
Priscilla-22 <priscillamw123@gmail.com>
2024-05-11 21:04:39 +0300

    feat:blog page

--- fundraiser/src/components/DonationPage/BlogList.jsx
+++ fundraiser/src/components/DonationPage/BlogList.jsx
@@ -1,11 +1,11 @@
+// bloglist.jsx
 import React from 'react';
 import BlogPreview, { blogs } from './BlogPreview';
 
 const BlogList = () => {
   return (
-    <div className='container flex rounded-xl border border-black-600 mx-auto mt-8 p-4'>
-      <div className=' '>
-        <h1 className='text-orange-700 font-bold text-4xl mx-auto text-center'>BLOGS.</h1>
+    <div className='container  mx-auto mt-8 p-4'>
+      <div className='flex flex-wrap justify-center'>
         {blogs.map((blog, index) => (
           <BlogPreview key={index} {...blog} index={index} />
         ))}
--- fundraiser/src/components/DonationPage/BlogPreview.jsx
+++ fundraiser/src/components/DonationPage/BlogPreview.jsx
@@ -1,8 +1,9 @@
+//blogpreview.jsx
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import '../../index.css'
 const BlogPreview = ({ title, image, timeToRead, description, index }) => {
-  const isEven = index % 2 === 0; // Check if the index is even
+  const isEven = index % 2 === 0; 
   const [expanded, setExpanded] = useState(false);
   const [prevDescription, setPrevDescription] = useState(description);
 
@@ -14,32 +15,90 @@ const BlogPreview = ({ title, image, timeToRead, description, index }) => {
   };
 
   return (
-    <div className={`bg-white shadow-md rounded-lg p-6 mb-6 flex ${isEven ? 'flex-row-reverse' : 'flex-row'}`}>
-      <div className=' mb-4'>
-        <img src={image} alt='Blog' className='w-[370px] h-[250px]' />
-      </div>
-      <div className='flex-1 mt-7 m-7'>
-        <h2 className='text-2xl font-bold mb-2'>{title}</h2>
-        <p className='text-gray-600 text-sm mb-4'>
-          Read time:
-          <span className='text-orange-500'>{` ${timeToRead} min`}</span>
-        </p>
-        <p className='text-gray-700 mb-4'>{expanded ? description : `${prevDescription.slice(0, 150)}...`}</p>
-        {!expanded && (
-          <button onClick={toggleExpanded} className='text-orange-500 font-bold py-2 px-4 rounded'>
-            Read More
-          </button>
-        )}
-        {expanded && (
-          <span className='text-orange-500 font-bold py-2 px-4 rounded cursor-pointer' onClick={toggleExpanded}>
-            Read Less
-          </span>
-        )}
-       
-      </div>
+    <div
+      className={`bg-white shadow-md rounded-lg p-6 mb-6 ${
+        index === 0 ? 'flex justify-center w-full' : 'w-1/2 md:w-1/3 lg:w-1/4'
+      }`}
+    >
+      {index === 0 ? (
+        <div className='flex flex-row-reverse'>
+          <div className='mb-4'>
+            <img src={image} alt='Blog' className='w-[370px] h-[250px]' />
+          </div>
+          <div className='flex-1 mt-7 m-7'>
+            <h2 className='text-2xl font-bold mb-2'>{title}</h2>
+            <p className='text-gray-600 text-sm mb-4'>
+              Read time:
+              <span className='text-orange-500'>{` ${timeToRead} min`}</span>
+            </p>
+            <p className='text-gray-700 mb-4'>
+              {expanded ? description : `${prevDescription.slice(0, 150)}...`}
+            </p>
+            {!expanded && (
+              <button
+                onClick={toggleExpanded}
+                className='text-orange-500 font-bold py-2 px-4 rounded'
+              >
+                Read More
+              </button>
+            )}
+            {expanded && (
+              <span
+                className='text-orange-500 font-bold py-2 px-4 rounded cursor-pointer'
+                onClick={toggleExpanded}
+              >
+                Read Less
+              </span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div
+          className='bg-cover bg-center h-90 relative blur-custom'
+          style={{ backgroundImage: `url(${image})`, height: '450px' }}
+        >
+          <div
+            className='absolute inset-0 z-0'
+            style={{
+              backgroundImage: `url(${image})`,
+              opacity: 0.9,
+              filter: 'blur(5px)',
+            }}
+          ></div>
+
+          <div className='absolute top-0 left-0 w-full h-full blur mt-8'></div>
+          <div className='p-4 text-black absolute top-0 left-0'>
+            <h2 className='text-2xl font-bold mb-2'>{title}</h2>
+            <p className='text-gray-700 text-sm mb-4'>
+              Read time:
+              <span className='text-orange-500'>{` ${timeToRead} min`}</span>
+            </p>
+            <p className='text-gray-700 mb-4'>
+              {expanded ? description : `${prevDescription.slice(0, 150)}...`}
+            </p>
+            {!expanded && (
+              <button
+                onClick={toggleExpanded}
+                className='text-orange-500 font-bold py-2 px-4 rounded'
+              >
+                Read More
+              </button>
+            )}
+            {expanded && (
+              <span
+                className='text-orange-500 font-bold py-2 px-4 rounded cursor-pointer'
+                onClick={toggleExpanded}
+              >
+                Read Less
+              </span>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 
 
 
@@ -63,6 +122,13 @@ export const blogs = [
     image: 'https://source.unsplash.com/random/600x400',
     timeToRead: '3',
     description: "This is another blog post. It's about something else.",
+  },
+  {
+    title: 'Combating Environmental Degradation: Encouraging Repeat Donations',
+    image: 'https://source.unsplash.com/random/600x400',
+    timeToRead: '5',
+    description:
+      'Environmental degradation is the disintegration of the earth or deterioration of the environment through the consumption of assets, for example, air, water, and soil; the destruction of environments and the eradication of wildlife. It is characterized as any change or aggravation to nature’s turf seen to be pernicious or undesirable. This process leads to the endangerment of the human race, however, there are limited resources to help combat this at this time. One of the ways the organization is looking to raise funds is by encouraging repeat donations. The goal would be to create a platform that allows users to donate to organizations such as this one regularly, to enable the organization to meet their goals. They are looking to create a product that automates the donation process by allowing donors to set up regular donations e.g. monthly donations of stipulated amounts.',
   },
 ];
 
