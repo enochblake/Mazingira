c939dd10e5ae7f96c34fc7c05449081120fccb99
Priscilla-22 <priscillamw123@gmail.com>
2024-05-12 18:04:02 +0300

    Refactor: Extract donation amount calculation to separate function in DonationAmount component

--- fundraiser/src/components/AddProduct/DonationAmount.jsx
+++ fundraiser/src/components/AddProduct/DonationAmount.jsx
@@ -16,16 +16,16 @@ const DonationAmount = () => {
       const response = await axios.get('http://localhost:5000/donationAmount');
       const data = response.data;
       setDonors(data);
-      const totalAmount = data.reduce((acc, donor) => acc + donor.amount, 0);
-      setTotalAmount(totalAmount);
-      const totalOrganizationAmount = data.reduce(
-        (acc, donor) => acc + donor.amount,
-        0
-      );
-      setTotalOrganizationAmount(totalOrganizationAmount);
+      calculateTotals(data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+  };
+
+  const calculateTotals = (data) => {
+    const totalAmount = data.reduce((acc, donor) => acc + donor.amount, 0);
+    setTotalAmount(totalAmount);
+    setTotalOrganizationAmount(totalAmount);
   };
 
   const handleTypeChange = (type) => {
@@ -46,7 +46,7 @@ const DonationAmount = () => {
 
   return (
     <div className='container mx-auto p-4'>
-      <div className='flex  mb-4'>
+      <div className='flex mb-4'>
         <span
           className={`px-4 py-2 cursor-pointer ${
             activeType === 'non-anonymous'
@@ -68,25 +68,39 @@ const DonationAmount = () => {
           Anonymous
         </span>
       </div>
-      <div className='flex'>
-        <div className='w-1/2'>
+      <div className='flex '>
+        <div className='w-full'>
           <h2 className='text-lg font-bold'>
             {activeType === 'non-anonymous'
               ? 'Non-Anonymous Donors'
               : 'Anonymous Donors'}
           </h2>
           <ul>
-            {filteredDonors.map((donor) => (
-              <li key={donor.id}>
-                {donor.anonymous
-                  ? 'Anonymous'
-                  : `${donor.firstName} ${donor.lastName}`}
-                : ${donor.amount} (Donated on: {donor.date})
-              </li>
+            {filteredDonors.map((donor, index) => (
+              <React.Fragment key={donor.id}>
+                <li className='flex'>
+                  <div>
+                    <p>
+                      {donor.anonymous
+                        ? 'Anonymous'
+                        : `${donor.firstName} ${donor.lastName}`}
+                    </p>
+                    <p>Donated on: {donor.date}</p>{' '}
+                  </div>
+                  <li className='ml-[400px]'>
+                  <div className='text-orange-500 text-lg'>${donor.amount}</div>
+                  </li>
+                </li>
+                {index !== filteredDonors.length - 1 && (
+                  <li>
+                    <hr className='w-full my-2 border-gray-400' />
+                  </li>
+                )}
+              </React.Fragment>
             ))}
           </ul>
         </div>
-        <div className='w-1/2'>
+        <div className='w-1/2 ml-6 '>
           <div className='bg-white p-4 rounded shadow-md mb-4'>
             <h2 className='text-lg font-bold'>Total Amount</h2>
             <p className='text-2xl'>${totalAmountForType}</p>
