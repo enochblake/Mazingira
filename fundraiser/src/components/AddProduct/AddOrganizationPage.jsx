7673a8a2a634cd9854bf5558bcd0b3a40a4b3bc3
Priscilla-22 <priscillamw123@gmail.com>
2024-05-12 13:01:16 +0300

    refactor:Updated TextareaField component in AddOrganizationPage with new classname and resize-none property

--- fundraiser/src/components/AddProduct/AddOrganizationPage.jsx
+++ fundraiser/src/components/AddProduct/AddOrganizationPage.jsx
@@ -201,7 +201,7 @@ const TextareaField = ({ label, name, value, onChange, maxLength }) => {
     <div className='mb-4'>
       <label className='block text-gray-700 font-bold mb-2'>{label}</label>
       <textarea
-        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40 resize-none'
         id={name}
         name={name}
         value={value}
