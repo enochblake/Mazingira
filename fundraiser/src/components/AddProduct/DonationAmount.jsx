7c6a2d8090f75b6cd8479f8562df8399c75270c8
Priscilla-22 <priscillamw123@gmail.com>
2024-05-12 18:51:38 +0300

    feat:Add donation amount page with total amount and donation type filtering; add 'Donate' button with link to pay page

--- fundraiser/src/components/AddProduct/DonationAmount.jsx
+++ fundraiser/src/components/AddProduct/DonationAmount.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 const DonationAmount = () => {
   const [donors, setDonors] = useState([]);
@@ -45,10 +47,13 @@ const DonationAmount = () => {
   );
 
   return (
-    <div className='container mx-auto p-4'>
-      <div className='flex mb-4'>
+    <div className='container mx-auto p-4 '>
+      <h1 className='text-4xl font-bold text-center mb-4 text-orange-700'>
+        Donation Amount Page
+      </h1>
+      <div className='flex mb-4 mt-7'>
         <span
-          className={`px-4 py-2 cursor-pointer ${
+          className={`px-4 py-2 cursor-pointer text-lg  ${
             activeType === 'non-anonymous'
               ? 'text-black border-b-4 border-orange-500'
               : ''
@@ -58,7 +63,7 @@ const DonationAmount = () => {
           Non-Anonymous
         </span>
         <span
-          className={`px-4 py-2 cursor-pointer ${
+          className={`px-4 py-2 cursor-pointer text-lg ${
             activeType === 'anonymous'
               ? 'text-black border-b-4 border-orange-500'
               : ''
@@ -75,21 +80,23 @@ const DonationAmount = () => {
               ? 'Non-Anonymous Donors'
               : 'Anonymous Donors'}
           </h2>
-          <ul>
+          <ul className='w-1/2'>
             {filteredDonors.map((donor, index) => (
               <React.Fragment key={donor.id}>
-                <li className='flex'>
+                <li className='flex items-center py-4'>
                   <div>
-                    <p>
+                    <p className='text-lg'>
                       {donor.anonymous
                         ? 'Anonymous'
                         : `${donor.firstName} ${donor.lastName}`}
                     </p>
-                    <p>Donated on: {donor.date}</p>{' '}
+                    <p className='text-gray-400 text-lg'>
+                      Donated on: {donor.date}
+                    </p>{' '}
                   </div>
-                  <li className='ml-[400px]'>
-                  <div className='text-orange-500 text-lg'>${donor.amount}</div>
-                  </li>
+                  <div className='text-orange-500 text-xl font-bold ml-auto'>
+                    ${donor.amount}
+                  </div>
                 </li>
                 {index !== filteredDonors.length - 1 && (
                   <li>
@@ -100,14 +107,24 @@ const DonationAmount = () => {
             ))}
           </ul>
         </div>
+
         <div className='w-1/2 ml-6 '>
-          <div className='bg-white p-4 rounded shadow-md mb-4'>
-            <h2 className='text-lg font-bold'>Total Amount</h2>
-            <p className='text-2xl'>${totalAmountForType}</p>
+          <div className='bg-white p-16 rounded shadow-lg mb-4'>
+            <h2 className='text-lg font-bold mx-auto text-center'>
+              Total Amount
+            </h2>
+            <p className='text-2xl text-center'>${totalAmountForType}</p>
           </div>
-          <div className='bg-white p-4 rounded shadow-md'>
-            <h2 className='text-lg font-bold'>Total Organization Amount</h2>
-            <p className='text-2xl'>${totalOrganizationAmount}</p>
+          <div className='bg-white p-16 rounded shadow-lg flex flex-col items-center'>
+            <h2 className='text-lg font-bold text-center'>
+              Total Organization Amount
+            </h2>
+            <p className='text-2xl text-center'>${totalOrganizationAmount}</p>
+            <Link to='/pay-page'>
+              <button className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-3 px-5 rounded mt-4'>
+                Donate
+              </button>
+            </Link>
           </div>
         </div>
       </div>
