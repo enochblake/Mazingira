02a0f6c797e941d762343a8aa48ca5badf704ac6
Priscilla-22 <priscillamw123@gmail.com>
2024-05-12 19:02:44 +0300

    refactor: Add gray background to BlogList container and update BlogPreview classNames for consistent styling

--- fundraiser/src/components/DonationPage/BlogList.jsx
+++ fundraiser/src/components/DonationPage/BlogList.jsx
@@ -4,11 +4,11 @@ import BlogPreview, { blogs } from './BlogPreview';
 
 const BlogList = () => {
   return (
-    <div>
+    <div className='bg-gray-200 ' >
       <h1 className='text-orange-700 text-4xl font-bold mx-auto text-center'>
         BLOGS.
       </h1>
-      <div className='container  mx-auto mt-8 p-4'>
+      <div className='container mx-auto mt-8 p-4'>
         <div className='flex flex-wrap justify-center'>
           {blogs.map((blog, index) => (
             <BlogPreview key={index} {...blog} index={index} />
