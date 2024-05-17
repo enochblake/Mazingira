173325a5a9f4f668f4d58ec6344ca49b40057dc2
Priscilla-22 <priscillamw123@gmail.com>
2024-05-17 19:17:49 +0300

    fix:Remove content switching functionality, simplified layout, and refactored code

--- fundraiser/src/components/BeneficiaryInventory/BeneficiaryInventory.jsx
+++ fundraiser/src/components/BeneficiaryInventory/BeneficiaryInventory.jsx
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import AddBeneficiary from './AddBeneficiary';
-import config from '../../config'
+import config from '../../config';
 
 const BeneficiaryInventory = () => {
   const [inventory, setInventory] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
-  // const [selectedContent, setSelectedContent] = useState('inventory');
 
   useEffect(() => {
     fetchData();
@@ -15,7 +14,9 @@ const BeneficiaryInventory = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`${config.baseURL}/beneficiary`, {withCredentials: true});
+      const response = await axios.get(`${config.baseURL}/beneficiary`, {
+        withCredentials: true,
+      });
       const beneficiaryData = response.data;
       setInventory(beneficiaryData);
       calculateTotalAmount(beneficiaryData);
@@ -32,96 +33,62 @@ const BeneficiaryInventory = () => {
     setTotalAmount(total);
   };
 
-  // const handleContentChange = (content) => {
-  //   setSelectedContent(content);
-  // };
-
   return (
     <div className='bg-gray-100 '>
-      <h1 className='text-4xl font-bold text-center mb-4 text-orange-700'>
-        Beneficiary & Inventory
-      </h1>
-      <div className='flex justify-center my-4'>
-        {/* <Link
-          to='#'
-          className={`mx-4 text-black  font-bold text-lg ${
-            selectedContent === 'inventory'
-              ? 'border-b-4 border-orange-500'
-              : ''
-          }`}
-          onClick={() => handleContentChange('inventory')}
-        >
-          Inventory
-        </Link> */}
-        {/* <Link
-          to='#'
-          className={`mx-4 text-black font-bold text-lg ${
-            selectedContent === 'beneficiaries'
-              ? 'border-b-4 border-orange-500'
-              : ''
-          }`}
-          onClick={() => handleContentChange('beneficiaries')}
-        >
-          Beneficiaries
-        </Link> */}
-      </div>
-
-      {/* {selectedContent === 'inventory' && ( */}
-        <div className='bg-gray-700 min-h-screen  p-16 mx-10 mb-2'>
-          <div className='container mx-auto border shadow-md mt-4 bg-gray-100 w-4/7 justify-center text-center p-4 pl-5'>
-            <div className='flex mb-4 mt-7'>
-              <h2 className='text-lg font-bold'>Beneficiaries</h2>
-            </div>
-            <div className='flex'>
-              <div className='w-full'>
-                <div className='max-h-96 overflow-y-auto'>
-                  <ul className='w-3/4'>
-                    {inventory.map((item, index) => (
-                      <React.Fragment key={index}>
-                        <li className='flex py-4'>
-                          <div>
-                            <img
-                              src={item.image_url}
-                              alt={item.name}
-                              className='w-12 h-12 rounded-full'
-                            />
-                            <p className='text-lg flex justify-start font-semibold ml-4'>
-                              {item.name}
-                            </p>
-                            <p className='text-gray-400 text-lg'>
-                              Donated on: {item.created_at}
-                            </p>
-                          </div>
-                          <div className='text-orange-500 text-xl font-bold ml-auto'>
-                            ${item.recieved_amount}
-                          </div>
+      <div className='bg-gray-700   p-10 mx-10 mb-2'>
+        <div className='container mx-auto border shadow-md  bg-gray-100 w-4/7 justify-center text-center p-4 pl-5'>
+          <h1 className='text-xl font-bold text-center mb-4 text-orange-700'>
+            List of Beneficiaries
+          </h1>
+          <div className='flex mb-4 mt-7'>
+            <h2 className='text-lg font-bold'>Beneficiaries</h2>
+          </div>
+          <div className='flex'>
+            <div className='w-full'>
+              <div className='max-h-96 overflow-y-auto'>
+                <ul className='w-3/4'>
+                  {inventory.map((item, index) => (
+                    <React.Fragment key={index}>
+                      <li className='flex py-4'>
+                        <div>
+                          <img
+                            src={item.image_url}
+                            alt={item.name}
+                            className='w-12 h-12 rounded-full'
+                          />
+                          <p className='text-lg flex justify-start font-semibold ml-4'>
+                            {item.name}
+                          </p>
+                          <p className='text-gray-400 text-lg'>
+                            Donated on: {item.created_at}
+                          </p>
+                        </div>
+                        <div className='text-orange-500 text-xl font-bold ml-auto'>
+                          ${item.recieved_amount}
+                        </div>
+                      </li>
+                      {index !== inventory.length - 1 && (
+                        <li>
+                          <hr className='w-full my-2 border-gray-400' />
                         </li>
-                        {index !== inventory.length - 1 && (
-                          <li>
-                            <hr className='w-full my-2 border-gray-400' />
-                          </li>
-                        )}
-                      </React.Fragment>
-                    ))}
-                  </ul>
-                </div>
+                      )}
+                    </React.Fragment>
+                  ))}
+                </ul>
               </div>
+            </div>
 
-              <div className='w-1/2 ml-6'>
-                <div className='total-Amount-container bg-white p-16 rounded shadow-lg mb-4'>
-                  <h2 className='text-lg font-bold mx-auto text-center'>
-                    Total Amount
-                  </h2>
-                  <p className='text-2xl text-center'>${totalAmount}</p>
-                </div>
-             
+            <div className='w-1/2 ml-6'>
+              <div className='total-Amount-container bg-white p-16 rounded shadow-lg mb-4'>
+                <h2 className='text-lg font-bold mx-auto text-center'>
+                  Total Amount
+                </h2>
+                <p className='text-2xl text-center'>${totalAmount}</p>
               </div>
             </div>
           </div>
         </div>
-      {/* )} */}
-
-      {/* {selectedContent === 'beneficiaries' && <AddBeneficiary />} */}
+      </div>
     </div>
   );
 };
