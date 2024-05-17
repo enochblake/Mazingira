15c74a2160c71bc93813f137f465ded86ee59bb8
Priscilla-22 <priscillamw123@gmail.com>
2024-05-17 19:10:01 +0300

    style: change 'pt-10 pb-10' to 'p-10' in outer div."

--- fundraiser/src/components/BeneficiaryInventory/AddBeneficiary.jsx
+++ fundraiser/src/components/BeneficiaryInventory/AddBeneficiary.jsx
@@ -42,7 +42,7 @@ const AddBeneficiary = () => {
   };
 
   return (
-    <div className='bg-gray-700 mt-5 pt-10 pb-10 mx-10 '>
+    <div className='bg-gray-700 mt-5 p-10 mx-10 '>
       <div className='container mx-auto py-8 bg-gray-100 rounded shadow-md p-5 mb-5 '>
         <h1 className='text-2xl font-bold mb-5 mx-auto text-center text-orange-700'>
           CREATE BENEFICIARY 
