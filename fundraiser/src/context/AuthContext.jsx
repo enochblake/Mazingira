fdf79460fea1ec1cb71899d06e4155d5e41b6a60
John <johntaykimani@gmail.com>
2024-05-18 15:13:01 +0300

    feat: Added checksession to the auth useContext.

--- fundraiser/src/context/AuthContext.jsx
+++ fundraiser/src/context/AuthContext.jsx
@@ -1,9 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import config from '../config';
+import axios from 'axios'
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [authenticated, setAuthenticated] = useState(false);
+
+    // Function to check session status
+    const checkSession = async () => {
+        try {
+            const response = await axios.get(`${config.baseURL}/checksession`, { withCredentials: true });
+            if (response.data.authenticated) {
+                setAuthenticated(true);
+            } else {
+                setAuthenticated(false);
+            }
+        } catch (error) {
+            console.error('Session check failed:', error);
+            setAuthenticated(false);
+        }
+    };
+
+    // Check session status when component mounts
+    useEffect(() => {
+        checkSession();
+    }, []);
 
     return (
         <AuthContext.Provider value={{ authenticated, setAuthenticated }}>
